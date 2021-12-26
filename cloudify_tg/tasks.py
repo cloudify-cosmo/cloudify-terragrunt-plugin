f1442c7a67b06dea9c65a65b49297bedd66c648e
nely <nely@cloudify.co>
2021-12-26 15:58:16 +0200

    first step

--- cloudify_tg/tasks.py
+++ cloudify_tg/tasks.py
@@ -32,9 +32,48 @@ def delete(tg, **_):
 
 @operation
 @decorators.with_terragrunt
+def terragrunt_plan(tg, **_):
+    tg.plan()
+
+
+@operation
+@decorators.with_terragrunt
+def terragrunt_destroy(tg, **_):
+    tg.destroy()
+
+@operation
+@decorators.with_terragrunt
+def terragrunt_output(tg, **_):
+    tg.terraform_output()
+
+@operation
+@decorators.with_terragrunt
+def terragrunt_apply(tg, **_):
+    tg.apply()
+
+
+@operation
+@decorators.with_terragrunt
+def terragrunt_info(tg, **_):
+    tg.terragrunt_info()
+
+
+@operation
+@decorators.with_terragrunt
+def validate_inputs(tg, **_):
+    tg.validate_inputs()
+
+
+@operation
+@decorators.with_terragrunt
 def graph_dependencies(tg, **_):
     tg.graph_dependencies()
 
+
+@operation
+@decorators.with_terragrunt
+def render_json(tg, **_):
+    tg.render_json()
 
 @operation
 @decorators.with_terragrunt

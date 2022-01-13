a475d913c0c32d54dca55326eccc67bbfb2a48dc
nely <nely@cloudify.co>
2022-01-13 14:51:47 +0200

    func

--- tg_sdk/test_scripts/test_tg.py
+++ tg_sdk/test_scripts/test_tg.py
@@ -12,7 +12,7 @@ init_params = {
             'environment_variables': {
                 'AWS_ACCESS_KEY_ID': os.environ['AWS_ACCESS_KEY_ID'],
                 'AWS_SECRET_ACCESS_KEY': os.environ['AWS_SECRET_ACCESS_KEY'],
-                'AWS_DEFAULT_REGION': os.environ['eu-west-1']
+                'AWS_DEFAULT_REGION': os.environ['AWS_DEFAULT_REGION']
             },
             'run_all': False,
             'command_options': {
@@ -28,5 +28,16 @@ init_params = {
 
 if __name__ == '__main__':
     terragrunt = tg.Terragrunt(**init_params)
-    terragrunt.terragrunt_info()
+    # terragrunt.terraform_output()
+    # terragrunt.terragrunt_info()
+    # terragrunt.validate_inputs()
+    # terragrunt.graph_dependencies()
+    # terragrunt.render_json()
+    #
+    # terragrunt.terraform_plan()
+    # terragrunt.apply()
+    # terragrunt.destroy()
+    # terragrunt.output()
+
+
 
